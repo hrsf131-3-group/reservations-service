1d5f783801ab618f644fbc40caa5e12f3cc39057
Linh Dang <linh.dang01@gmail.com>
2020-11-23 15:24:59 -0800

    refactored guestDropdown to styled

--- client/src/components/guestDropdown.jsx
+++ client/src/components/guestDropdown.jsx
@@ -1,22 +1,40 @@
 import React from 'react'
 import GuestPicker from './guestPicker.jsx'
+import styled from 'styled-components'
+
+const GuestDropdownStyle = styled.div`
+  cursor: pointer;
+  justify-content: flex-end;
+`;
+const GuestCountTitle = styled.section`
+  padding-top: 10px;
+  padding-left: 10px;
+  font-weight: bold;
+  font-size: 8px;
+`;
+const GuestCountDisplay = styled.section`
+  border-style: none;
+  padding-left: 10px;
+  padding-bottom: 10px;
+  font-size: 14px;
+  background: transparent;
+  border: none;
+  font-weight: 300;
+`;
 
 function GuestDropdown(props) {
   return (
     <div>
-      <div
-        className="guestDropdown"
+      <GuestDropdownStyle
         onClick={props.displayGuestPickerOnClick}>
-        <section className="guestCountTitle">GUESTS</section>
-        <section className="guestCountDisplay">{props.guestCount.adults} {props.guestCount.adults === 1 ? 'guest' : 'guests'}</section>
-      </div>
-      <div>
-        <GuestPicker
-          maxGuestCount={props.maxGuestCount}
-          isGuestDropdownDisplay={props.isGuestDropdownDisplay}
-          displayGuestPickerOnClick={props.displayGuestPickerOnClick}
-        />
-      </div>
+        <GuestCountTitle>GUESTS</GuestCountTitle>
+        <GuestCountDisplay>{props.guestCount.adults} {props.guestCount.adults === 1 ? 'guest' : 'guests'}</GuestCountDisplay>
+      </GuestDropdownStyle>
+      <GuestPicker
+        maxGuestCount={props.maxGuestCount}
+        isGuestDropdownDisplay={props.isGuestDropdownDisplay}
+        displayGuestPickerOnClick={props.displayGuestPickerOnClick}
+      />
     </div>
   )
 }
