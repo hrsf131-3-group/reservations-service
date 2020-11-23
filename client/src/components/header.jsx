dadb12b21b8aed38c282eb93507d255cb0c7e473
Linh Dang <linh.dang01@gmail.com>
2020-11-23 10:00:19 -0800

    refactored header to styled

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const HeaderPricePerNight = styled.div`
+  justify-content: flex-start;
+  margin-bottom: 20px;
+`;
+
+const PricePerNight = styled.span`
+  font-weight: 600;
+  font-size: 20px;
+`;
 
 function Header(props) {
   return (
-    <div className="headerPricePerNight">
-      <span className="pricePerNight">{`$${props.pricePerNight}`}</span>
-      <span className="perNightString">{` / night`}</span>
-    </div>
+    <HeaderPricePerNight>
+      <PricePerNight>{`$${props.pricePerNight}`}</PricePerNight>
+      <PricePerNight>{` / night`}</PricePerNight>
+    </HeaderPricePerNight>
   )
 }
 
